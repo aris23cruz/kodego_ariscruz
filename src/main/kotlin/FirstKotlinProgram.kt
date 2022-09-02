453856e4bfda5dd38df9da6d3ebbb86e284697e1
aris cruz <ariscruz.va@gmail.com>
2022-09-02 21:09:50 +0800

    4th commit

--- src/main/kotlin/FirstKotlinProgram.kt
+++ src/main/kotlin/FirstKotlinProgram.kt
@@ -4,10 +4,12 @@ fun main(){
     Date Created: 9/1/2022
     Filename: FirstKotlinProgram.kt
      */
-    print("Hello World!")
-    print("Hello World!!")
-    print("Hello World!!!!")
-    print("Hello World4")
-    print("Hello World5")
+  println("Enter your number 1:")
+    var number1 : Int = readln().toInt()
 
+    println("Enter number 2:")
+    var number2 : Int = readln().toInt()
+
+    var sum : Int = number1 + number2
+    print("My Name is $sum")
 }
