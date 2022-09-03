13f5ad46383bcc4ad35a09d967bdebdbcf53c12f
aris cruz <ariscruz.va@gmail.com>
2022-09-03 13:11:08 +0800

    PractiseActivity1.kt

---
+++ src/main/kotlin/practise_activities/PracticeActivity1.kt
@@ -0,0 +1,16 @@
+package practise_activities
+
+fun main(){
+
+    println("Enter Your FirstName")
+    var FirstName: String = readln().toString()
+    println("$FirstName")
+    println("Enter Your Middle Name/Initial")
+    var MiddleName: String = readln().toString()
+    println("$MiddleName")
+    println("Enter Your Last Name")
+    var LastName: String = readln().toString()
+    println("$LastName")
+    println("Your Name is $FirstName $MiddleName $LastName")
+
+}
