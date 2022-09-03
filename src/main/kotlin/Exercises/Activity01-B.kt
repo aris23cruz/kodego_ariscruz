176074a0335692f5a98063d32a94217f96f617f1
aris cruz <ariscruz.va@gmail.com>
2022-09-03 12:18:21 +0800

    Activity01-B.kt

---
+++ src/main/kotlin/Exercises/Activity01-B.kt
@@ -0,0 +1,30 @@
+package Exercises
+
+fun main(){
+    println("List of Books")
+      println("History")
+      println("Science")
+      println("Sci-fi")
+      println("Novel")
+    println("Item Borrowed")
+    var item: String = readln().toString()
+    println("$item")
+    println("Book Number")
+    var number1: Int = readln().toInt()
+    print("$number1")
+    println("Date Borrowed")
+    var date1: String = readln().toString()
+    println("Date is $date1")
+    println("Number of Item Borrowed")
+    var number2: Int = readln().toInt()
+    println("Number of Items $number2")
+    println("Date of Returned")
+    var date2: String = readln().toString()
+    println("Date is $date2")
+
+    println("Please return the book on due date. Happy reading!")
+
+
+
+
+}
