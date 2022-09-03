a20e53808f0dafba32b9dd8aa6e49b6392e2ce48
aris cruz <ariscruz.va@gmail.com>
2022-09-03 17:11:00 +0800

    KotlinProgram5.kt

---
+++ src/main/kotlin/KotlinProgram5.kt
@@ -0,0 +1,10 @@
+fun main(){
+    var itemNumber:Int = 2
+
+    when(itemNumber){
+        1 -> print("Clothes")
+        2 -> print("food")
+        3 -> print("toilettries")
+        else -> print("invalid item number")
+    }
+}
