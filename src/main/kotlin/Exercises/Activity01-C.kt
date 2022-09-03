90eab1091a57c9029a6f4b6ffc793ff2c2061570
aris cruz <ariscruz.va@gmail.com>
2022-09-03 13:01:52 +0800

    Activity01-C.kt

---
+++ src/main/kotlin/Exercises/Activity01-C.kt
@@ -0,0 +1,11 @@
+package Exercises
+
+fun main(){
+
+    println("List of Items")
+    var item1: String = "Can Goods"
+    var item2: String = "Beverages"
+    var item3: String = "Meat"
+    var item4: String = "Dry Goods"
+
+}
