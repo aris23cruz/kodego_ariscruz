065d1e8ca29d5e8c2c7dd8080eeff2d44bef206b
aris cruz <ariscruz.va@gmail.com>
2022-09-03 10:34:49 +0800

    sample practise for KotlinProgram2.kt

---
+++ src/main/kotlin/KotlinProgram2.kt
@@ -0,0 +1,26 @@
+fun main(){
+    var string1 : String = "this is my String"
+    var string2 : String = "this is my String too!"
+    //string character
+    println(string1[11])
+    //string length
+    println(string1.length)
+    println(string2.length)
+    //string equality
+    println(string1 == string2)
+    //string uppercase/lowercase
+    println(string1.uppercase())
+    //string uppercase/lowercase
+    println(string1.lowercase())
+    //startswith
+    println(string1.startsWith("this"))
+    //endswith
+    println(string2.endsWith("too!"))
+    //concatenate
+    println(string1+""+string2)
+    //substring
+    println(string1.substring(6,10))
+    //replace
+    println(string1.replace("this","that"))
+
+}
