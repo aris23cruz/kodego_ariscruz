7b95d7521a6a69948a3512c0a633007cc5576de0
aris cruz <ariscruz.va@gmail.com>
2022-09-03 11:27:05 +0800

    Activity01-A.kt

---
+++ src/main/kotlin/Exercises/Activity01-A.kt
@@ -0,0 +1,14 @@
+package Exercises
+
+fun main(){
+    println("Enter your Name")
+    var name: String = readln()
+    println("$name")
+    println("Attendance")
+    var attendance : String = readln()
+    println("$attendance")
+    println("Grade")
+    var number : Int = readln().toInt()
+    println("$number")
+
+}
