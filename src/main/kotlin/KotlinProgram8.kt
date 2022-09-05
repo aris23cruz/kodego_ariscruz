9e41417e42b927e21c903dcc3964030a98e48f30
aris cruz <ariscruz.va@gmail.com>
2022-09-05 20:16:20 +0800

    KotlinProgram8.kt

---
+++ src/main/kotlin/KotlinProgram8.kt
@@ -0,0 +1,13 @@
+fun main(){
+    // ctr++ 0r ctr-- (post decrement)
+    //while loop
+    //init
+    var ctr : Int = 1
+//
+    //condition
+    while(ctr <=5){
+        println("Hello World! $ctr")
+        //update
+        ctr++
+    }
+}
