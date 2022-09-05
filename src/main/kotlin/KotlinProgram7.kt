3231b89baf80659cc809000aafe9db35dd0c9559
aris cruz <ariscruz.va@gmail.com>
2022-09-05 20:18:50 +0800

    KotlinProgram7.kt

---
+++ src/main/kotlin/KotlinProgram7.kt
@@ -0,0 +1,9 @@
+fun main(){
+    repeat(5){
+        println("Hello World")
+    }
+    //repeat ctr is a variable and be changed. 5 is the number of iteration.
+    repeat(5){ ctr ->
+        println("Hello World $ctr")}
+
+}
