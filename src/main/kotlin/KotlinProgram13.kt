1a473e7c5ccf4832d8a7adc8ecc88254d2787893
aris cruz <ariscruz.va@gmail.com>
2022-09-07 20:03:07 +0800

    ArraysKotlinProgram13.kt

--- src/main/kotlin/KotlinProgram13.kt
+++ src/main/kotlin/KotlinProgram13.kt
@@ -11,4 +11,16 @@ fun main(){
 
     //array size
     println(numbers.size)
+
+    //how to change the elements in an array
+    println(names[0])
+    println(names[1])
+    println(names[2])
+    //names[1] = "Matthews"
+    //another way
+    names.set(1,"Matthew")
+    println(names.get(0))
+    println(names.get(1))
+    println(names.get(2))
+
 }
