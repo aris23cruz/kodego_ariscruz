d3d325ae5b20b8fe0b062b03ffa3902a25510bde
aris cruz <ariscruz.va@gmail.com>
2022-09-08 08:12:07 +0800

    ArrayKotlinProgram14.kt

---
+++ src/main/kotlin/KotlinProgram14.kt
@@ -0,0 +1,23 @@
+fun main(){
+
+    var names = arrayOf<String> ("John","Paul","Mark","Ringo","George")
+    var ctr : Int = names.size
+    while (ctr < names.size){
+        println(names[ctr])
+
+        ctr++
+    }
+    //display new array values using readln()
+    var ctr : Int = 0
+    while (ctr < names.size) {
+        println("Enter new value for index $ctr")
+
+        ctr++
+    }
+    var ctr2 : Int = 0
+    while(ctr2 < names.size){
+        println(names[ctr2])
+        ctr2++
+    }
+
+}
