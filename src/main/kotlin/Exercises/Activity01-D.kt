58c76fec926dcbac457736c381c8dd1362bc6033
aris cruz <ariscruz.va@gmail.com>
2022-09-08 10:45:59 +0800

    Activity01-D.kt

--- src/main/kotlin/Exercises/Activity01-D.kt
+++ src/main/kotlin/Exercises/Activity01-D.kt
@@ -1,43 +1,27 @@
 package Exercises
 
-fun main(){
+fun main() {
 
-//    print("Enter Amount")
-//    var number1: Int = readln().toInt()
-//    var number2: Int = 0
-//    for(number in 1..100) {
-//        println("Do you want to add another amount?")
-//        number2 = number2 + number1
-//        var ansNo: String = readln().toString()
-//        if (ansNo != "No" && ansNo != "no")
-//            number2 += ansNo.toInt()
-//
-//        }else{
-//            println("Divide the value by how many?")
-//        var input2: Int = readln().toInt()
-//        var input3 = ((number1 + number2) /input2)
-//       println("$input3")
-
-
-    var number: Double = 0.0
-    var ans: String = ""
-    var sum: Double = 0.0
-    var difference: Int = 0
+    var amount: Double = 0.0
+    var answer: String = ""
+    var divisor: Int = 0
+    var total: Double = 0.0
+    var input: Int = 0
 
     do {
         print("Enter Amount:")
-        number = readln().toDouble()
+        input = readln().toInt()
+        amount += input
 
-        number += number
+        print("Do you want to add another amount?(yes/no)")
+        answer = readln().lowercase()
+    } while (answer == "yes")
 
-        print("Do you want to add another amount?(Yes/No)")
-        ans = readln()
+        println("Divide the value by how many?")
+        divisor= readln().toInt()
+        total= amount/divisor
+
+        println("The result is $total")
+
     }
-    while (ans == "Yes")
-    print("Divide the value by how many?")
-    difference = readln().toInt()
 
-    sum = number / difference
-    println("The result is $sum")
-
-}
