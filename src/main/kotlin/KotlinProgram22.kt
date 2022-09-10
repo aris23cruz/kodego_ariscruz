80b64e2771fab58ec8d2ccfa5ce41eaeac68206d
aris cruz <ariscruz.va@gmail.com>
2022-09-10 14:07:57 +0800

    KotlinProgram22.kt

--- src/main/kotlin/KotlinProgram22.kt
+++ src/main/kotlin/KotlinProgram22.kt
@@ -1,5 +1,20 @@
 fun main() {
 
+    println("Enter your name:")
+    var name : String = readln()
+
+    println("Enter your age:")
+    var age : Int = readln().toInt()
+
+    displayNameAndAge(name,age)
+//println ("Enter number1:")
+//    var number : Int = readln().toInt()
+//    println("The cube is ${number * number * number}")
+//
+//    println ("Enter number2:")
+//    var number2 : Int = readln().toInt()
+//    println("The cube is ${number2 * number2 * number2}")
+
 }
 //no return no parameter
 fun displayName() {
@@ -9,7 +24,14 @@ fun displayName() {
 //no return with 1 parameter
 fun displayMessage(msg: String){
     println("The message is \"$msg\"")
+
 }
+//no return with 2 parameters
+fun displayNameAndAge(name: String,age: Int) {
+    println("Your name is $name")
+    println("Your age is $age")
+
+
     //built-in/standard functions in Kotlin
     //abstraction
 
@@ -21,5 +43,4 @@ fun displayMessage(msg: String){
     //listOf()
 
     //user-defined function
-
 }
