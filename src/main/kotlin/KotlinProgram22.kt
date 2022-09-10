2e449a68c70256c181464c029163d8b89f733d8c
aris cruz <ariscruz.va@gmail.com>
2022-09-10 13:52:43 +0800

    KotlinProgram22.kt

---
+++ src/main/kotlin/KotlinProgram22.kt
@@ -0,0 +1,25 @@
+fun main() {
+
+}
+//no return no parameter
+fun displayName() {
+    println("Peter")
+
+}
+//no return with 1 parameter
+fun displayMessage(msg: String){
+    println("The message is \"$msg\"")
+}
+    //built-in/standard functions in Kotlin
+    //abstraction
+
+    //println()
+    //toInt()
+    //readln()
+    //main()
+    //arrayOf()
+    //listOf()
+
+    //user-defined function
+
+}
