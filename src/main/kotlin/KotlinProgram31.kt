d7d385dd897531d775e6ff4dc6612e9dd4a5088a
aris cruz <ariscruz.va@gmail.com>
2022-09-20 20:56:44 +0800

    Try-Catch_KotlinProgram31.kt

---
+++ src/main/kotlin/KotlinProgram31.kt
@@ -0,0 +1,20 @@
+fun main(){
+    try {
+        println("Enter number 1:")
+        var num1: Int = readln().toInt()
+    }catch (e:Exception){
+        println("Invalid input! Digits only!")
+    }
+    println("-End of Code-")
+
+
+}
+
+//Exception
+//- something that disrupts a normal flow of your code
+//recoverable - you handle exceptions
+
+//Error
+//unrecoverable
+
+//try - catch block
