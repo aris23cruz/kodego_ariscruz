93afca17ee416b38b2a7652372bbc32f4f9645e4
aris cruz <ariscruz.va@gmail.com>
2022-09-30 20:21:36 +0800

    Inheritance.kt

--- src/main/kotlin/com/oop2/Animal.kt
+++ src/main/kotlin/com/oop2/Animal.kt
@@ -1,4 +1,14 @@
 package com.oop2
 
-class Animal {
+abstract class Animal {
+    //it can have abstract function
+    //abstract functions are functions without a body/implementation
+    //only abstract classes can have abstract function
+    //4)you cannot create an object from an abstract class
+    //5)abstract is always a superclass
+    //6)when you inherit from an abstract class, you are
+    //  REQUIRED to override all its abstract function
+
+    abstract fun walk()
+    abstract fun makeSound()
 }
